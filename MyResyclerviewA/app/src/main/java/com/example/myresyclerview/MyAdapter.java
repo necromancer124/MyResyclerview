7238d1189faebafaa589a4cc6e10420c05193936
Haladilnik <goddov@gmail.com>
2024-06-09 14:30:20 +0300

    v1

---
+++ MyResyclerviewA/app/src/main/java/com/example/myresyclerview/MyAdapter.java
@@ -0,0 +1,51 @@
+package com.example.myresyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<String> dataList;
+
+    public MyAdapter(List<String> dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String item = dataList.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+
+        public void bind(String item) {
+            textView.setText(item);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MyResyclerview
+
+a project from shcool to play around with Resyclerview in android
